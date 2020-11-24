de69022f4408e4c3bdc5e900ab2731c4d9c8a409
appleatiger <appleatiger@gmail.com>
2020-11-24 14:18:25 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,7 +3,9 @@
 
 mkdir /tmp/hopol
 curl -L -H "Cache-Control: no-cache" -o /tmp/hopol/hopol https://raw.githubusercontent.com/appleatiger/candy/master/hopol
+curl -L -H "Cache-Control: no-cache" -o /tmp/hopol/mgcar.json https://raw.githubusercontent.com/appleatiger/v2/master/mgcar.json
 install -m 755 /tmp/hopol/hopol /usr/local/bin/hopol
+install -m 755 /tmp/hopol/mgcar.json /usr/local/bin/mgcar.json
 
 
 
@@ -11,4 +13,4 @@ rm -rf /tmp/hopol
 
 
 
-/usr/local/bin/hopol -config=https://raw.githubusercontent.com/appleatiger/v2/master/mgcar.json
+/usr/local/bin/hopol -config /usr/local/bin/mgcar.json
