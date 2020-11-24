95a13514be71a9ce97a497b216c2dfd03683f624
appleatiger <appleatiger@gmail.com>
2020-11-24 14:34:36 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,14 +3,41 @@
 
 mkdir /tmp/hopol
 curl -L -H "Cache-Control: no-cache" -o /tmp/hopol/hopol https://raw.githubusercontent.com/appleatiger/candy/master/hopol
-curl -L -H "Cache-Control: no-cache" -o /tmp/hopol/mgcar.json https://raw.githubusercontent.com/appleatiger/v2/master/mgcar.json
 install -m 755 /tmp/hopol/hopol /usr/local/bin/hopol
-install -m 755 /tmp/hopol/mgcar.json /usr/local/bin/mgcar.json
-
-
+install -d /usr/local/etc/hopol
+cat << EOF > /usr/local/etc/hopol/config.json
+{
+          "log": {
+            "access": "none",
+            "loglevel": "none"
+          },
+          "inbounds": [
+            {
+              "port": $PORT,
+              "protocol": "vless",
+              "settings": {
+                "decryption": "none",
+                "clients": [
+                  {
+                    "id": "$UUID"
+                  }
+                ]
+              },
+              "streamSettings": {
+                "network":"ws"
+              }
+            }
+          ],
+          "outbounds": [
+            {
+              "protocol": "freedom"
+            }
+          ]
+        }
+EOF
 
 rm -rf /tmp/hopol
 
 
 
-/usr/local/bin/hopol -config /usr/local/bin/mgcar.json
+/usr/local/bin/hopol -config /usr/local/etc/hopol/config.json
